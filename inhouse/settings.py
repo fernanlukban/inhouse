d2e4519156ec70f493c71d1490dd71cbbf0437d0
Fernan Lukban <fernanlukban@gmail.com>
2020-09-20 23:22:24 -0700

    env settings

--- inhouse/settings.py
+++ inhouse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "p*-mm7%8-%&@j+c9gyvn7*0#3ikh$2z^3!n^+^e)d1k)i7-ybt"
+SECRET_KEY = os.getenv("INHOUSE_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ["138.197.195.31", "fernyfoods.com"]
 
